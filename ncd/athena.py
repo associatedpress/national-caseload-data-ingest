632d55da5a03dd84c51ffeb05820558cfb5c690d
Justin Myers <jmyers@ap.org>
2018-01-11 18:02:27 -0600

    Add actual Athena query support
    Still need to upload data to the right place, though, and update
    downstream users of the `Athena` class.

--- ncd/athena.py
+++ ncd/athena.py
@@ -1,72 +1,100 @@
-# flake8: noqa
+from io import BytesIO, TextIOWrapper
+import logging
+import posixpath
+from time import sleep
+
 import boto3
 
 
-# Transcript of experimentation with a `SELECT 1;` query:
-# >>> athena = boto3.client('athena')
-# >>> start_response = client.start_query_execution(QueryString='SELECT 1;', QueryExecutionContext={'Database': 'ncd_test'})
-# Traceback (most recent call last):
-#   File "<stdin>", line 1, in <module>
-# NameError: name 'client' is not defined
-# >>> start_response = athena.start_query_execution(QueryString='SELECT 1;', QueryExecutionContext={'Database': 'ncd_test'})
-# Traceback (most recent call last):
-#   [...]
-#   File "/Users/idguser/.virtualenvs/ncd/lib/python3.5/site-packages/botocore/validate.py", line 291, in serialize_to_request
-#     raise ParamValidationError(report=report.generate_report())
-# botocore.exceptions.ParamValidationError: Parameter validation failed:
-# Missing required parameter in input: "ResultConfiguration"
-# >>> start_response = athena.start_query_execution(QueryString='SELECT 1;', QueryExecutionContext={'Database': 'ncd_test'}, ResultConfiguration={'OutputLocation': 's3://aws-athena-query-results-462561078695-us-east-2/query_test'})
-# Traceback (most recent call last):
-#   [...]
-#   File "/Users/idguser/.virtualenvs/ncd/lib/python3.5/site-packages/botocore/client.py", line 615, in _make_api_call
-#     raise error_class(parsed_response, operation_name)
-# botocore.errorfactory.InvalidRequestException: An error occurred (InvalidRequestException) when calling the StartQueryExecution operation: The S3 location provided to save your query results is invalid. Please check your S3 location is correct and is in the same region and try again. If you continue to see the issue, contact customer support for further assistance.
-# >>> start_response = athena.start_query_execution(QueryString='SELECT 1;', QueryExecutionContext={'Database': 'ncd_test'}, ResultConfiguration={'OutputLocation': 's3://aws-athena-query-results-462561078695-us-east-1/query_test'})
-# >>> start_response
-# {'ResponseMetadata': {'RequestId': '6020fd71-f62f-11e7-8b99-8dd87ec67ff5', 'HTTPStatusCode': 200, 'RetryAttempts': 0, 'HTTPHeaders': {'date': 'Wed, 10 Jan 2018 17:54:59 GMT', 'content-type': 'application/x-amz-json-1.1', 'connection': 'keep-alive', 'content-length': '59', 'x-amzn-requestid': '6020fd71-f62f-11e7-8b99-8dd87ec67ff5'}}, 'QueryExecutionId': 'ba61e860-6964-4e84-baa9-996a7f20fadb'}
-# >>> s3 = boto3.client('s3')
-# >>> s3.waiter_names
-# ['bucket_exists', 'bucket_not_exists', 'object_exists', 'object_not_exists']
-# >>> s3_object_exists_waiter = s3.get_waiter('object_exists')
-# >>> s3_object_exists_waiter.wait(Bucket='aws-athena-query-results-462561078695-us-east-1', Key='query_test/ba61e860-6964-4e84-baa9-996a7f20fadb.csv'
-# ... )
-# >>> s3_object_exists_waiter.wait(Bucket='aws-athena-query-results-462561078695-us-east-1', Key='query_test/ba61e860-6964-4e84-baa9-996a7f20fadb.csv.foo')
-# ^CTraceback (most recent call last):
-#   File "<stdin>", line 1, in <module>
-#   File "/Users/idguser/.virtualenvs/ncd/lib/python3.5/site-packages/botocore/waiter.py", line 53, in wait
-#     Waiter.wait(self, **kwargs)
-#   File "/Users/idguser/.virtualenvs/ncd/lib/python3.5/site-packages/botocore/waiter.py", line 331, in wait
-#     time.sleep(sleep_amount)
-# KeyboardInterrupt
-# >>>
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    '%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
 class Athena(object):
-    """TODO: Document this.
+    """Helper for Athena I/O.
+
+    Args:
+        data_bucket: A string name of an S3 bucket where we should store data
+            files.
+        results_bucket: A string name of an S3 bucket where Athena should store
+            CSVs of query results.
+        s3_prefix: A string prefix to use for data files' S3 keys.
+        db_name: A string database name to use when creating and querying
+            tables.
     """
-    def __init__(self, data_bucket=None, results_bucket=None, s3_prefix=None):
+    def __init__(
+            self, data_bucket=None, results_bucket=None, s3_prefix=None,
+            db_name=None):
         self._athena = boto3.client('athena')
-        self._s3 = boto3.client('s3')
+        self._s3 = boto3.resource('s3')
+
         self.data_bucket = data_bucket
         self.results_bucket = results_bucket
         self.s3_prefix = s3_prefix
+        self.db_name = db_name
+
+        self.logger = logger.getChild('Athena')
 
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     # -=-=-=-=-=-=-=-=-=-=-= PUBLIC METHODS FOLLOW =-=-=-=-=-=-=-=-=-=-=-=-=-=-
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-    def execute_query(self, sql_string):
-        """TODO: Document this.
+    def create_db(self):
+        """Create the database we want to use.
         """
-        pass
+        self.execute_query(
+            'CREATE DATABASE IF NOT EXISTS {0};'.format(self.db_name),
+            'default')
 
-    def prefix_for_table(self):
-        """TODO: Document this.
+    def execute_query(self, sql_string, db_name=None):
+        """Execute a query on Athena.
+
+        Args:
+            sql_string: A string SQL query to execute.
+
+        Returns:
+            A StringIO of CSV output from Athena.
         """
-        pass
+        db_for_query = self.db_name if db_name is None else db_name
+        start_response = self._athena.start_query_execution(
+            QueryString=sql_string,
+            QueryExecutionContext={'Database': db_for_query},
+            ResultConfiguration={
+                'OutputLocation': 's3://{results_bucket}/{s3_prefix}'.format(
+                    results_bucket=self.results_bucket,
+                    s3_prefix=self.s3_prefix)
+            })
 
-    def upload_data(self, table_name, file_obj):
-        """TODO: Document this.
+        query_execution_id = start_response['QueryExecutionId']
+        self.logger.debug('Started query ID {0}'.format(query_execution_id))
+
+        return self._results_for_query(query_execution_id)
+
+    def prefix_for_table(self, table_name):
+        """Create a full prefix for S3 keys for a given table.
+
+        Args:
+            table_name: A string table name.
+
+        Returns:
+            A string S3 prefix.
+        """
+        return posixpath.join(self.s3_prefix, self.db_name, table_name)
+
+    def upload_data(self, table_name, file_obj, district=None):
+        """Upload the given data to S3.
+
+        Args:
+            table_name: A string table name.
+            file_obj: A binary file-like object.
+            district: An optional string code for a federal judicial district;
+                provide this when DOJ splits up a table by district.
         """
         pass
 
@@ -74,7 +102,59 @@ class Athena(object):
     # -=-=-=-=-=-=-=-=-=-=- INTERNAL METHODS FOLLOW -=-=-=-=-=-=-=-=-=-=-=-=-=-
     # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-    def _wait_for_result(self, query_execution_id):
-        """TODO: Document this.
+    def _results_for_query(self, query_execution_id):
+        """Retrieve the results for the given query.
+
+        Args:
+            query_execution_id: A string execution ID.
+
+        Returns:
+            A text file-like object of CSV output from Athena.
         """
-        pass
+        result_bucket, result_key = self._wait_for_result(query_execution_id)
+        results_bytes = BytesIO()
+        self.logger.debug('Downloading results for query ID {0}'.format(
+            query_execution_id))
+        self._s3.Bucket(result_bucket).download_fileobj(
+            result_key, results_bytes)
+        self.logger.debug('Downloaded results for query ID {0}'.format(
+            query_execution_id))
+        results_bytes.seek(0)
+        results_text = TextIOWrapper(results_bytes, encoding='utf-8')
+        return results_text
+
+    def _wait_for_result(self, query_execution_id):
+        """Wait for a query to complete.
+
+        This method will block until the query has completed.
+
+        Args:
+            query_execution_id: A string execution ID.
+
+        Returns:
+            A tuple with two elements:
+            *   A string S3 bucket name where results are stored.
+            *   A string S3 key for the object containing results.
+        """
+        try:
+            sleep(0.5)
+            while True:
+                query_execution = self._athena.get_query_execution(
+                    QueryExecutionId=query_execution_id)
+                query_state = query_execution['QueryExecution'][
+                    'Status']['State']
+                if query_state != 'RUNNING':
+                    break
+                self.logger.debug('Waiting for results for query {0}'.format(
+                    query_execution_id))
+                sleep(5)
+
+            output_location = query_execution['QueryExecution'][
+                'ResultConfiguration']['OutputLocation']
+            location_components = output_location.split('/', maxsplit=3)
+
+            return (location_components[2], location_components[3])
+        except BaseException as e:  # Yes, really.
+            self._athena.stop_query_execution(
+                QueryExecutionId=query_execution_id)
+            raise e
