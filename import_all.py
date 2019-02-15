9e32fdf5a04ee2064d7b6bbd4057fc269298d1fb
Justin Myers <jmyers@ap.org>
2019-02-15 16:33:44 -0600

    Switch to synchronous APIs for full import script
    *   We'd only been handling one file at a time anyway, so we weren't
        really making much use of the async functionality.
    *   It made the processing order unpredictable, which made it impossible
        to resume reliably.
    *   We were encountering errors when one file would finish being ingested
        before the next was finished downloading.
    
    Based on all of that, it seemed better to handle all of this
    synchronously and in numerical order.

--- import_all.py
+++ import_all.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 from argparse import ArgumentParser
-import asyncio
 import logging
 import sys
 from tempfile import NamedTemporaryFile
 
-import aiohttp
 from lxml import etree
+import requests
 from sqlalchemy import create_engine
 
 from ncd.data_zip import DataZip
@@ -30,45 +29,40 @@ parser.add_argument(
     help='URL to a DOJ page of yearly or monthly data files')
 
 
-async def get_file_urls(file_listing_url, session):
+def get_file_urls(file_listing_url):
     """Determine which URLs need to be downloaded.
 
     Args:
         file_listing_url: A string URL to a page on the DOJ site.
-        session: An aiohttp.ClientSession to use.
 
     Returns:
         A tuple of string URLs to individual zip files.
     """
-    async with session.get(file_listing_url) as response:
-        raw_html = await response.text()
-        html = etree.HTML(raw_html)
+    r = requests.get(file_listing_url)
+    raw_html = r.text
+    html = etree.HTML(raw_html)
     links = html.cssselect('a[href$=".zip"]')
     return tuple(map(lambda link: link.attrib['href'], links))
 
 
-async def load_file_from_url(zip_file_url, engine, session):
+def load_file_from_url(zip_file_url, engine):
     """Download a data file and load it into a database.
 
     Args:
         zip_file_url: A string URL to an NCD data file.
         engine: A sqlalchemy.engine.Engine.
-        session: An aiohttp.ClientSession to use.
     """
     zip_file_basename = zip_file_url.split('/')[-1]
     logger.debug('About to download {0}'.format(zip_file_basename))
     with NamedTemporaryFile() as zip_file:
         chunk_size = 32768
-        async with session.get(zip_file_url, timeout=0) as response:
-            logger.debug('Saving {0} to {1}'.format(
-                zip_file_basename, zip_file.name))
-            while True:
-                chunk = await response.content.read(chunk_size)
-                if not chunk:
-                    logger.debug('Finished saving {0} to {1}'.format(
-                        zip_file_basename, zip_file.name))
-                    break
-                zip_file.write(chunk)
+        r = requests.get(zip_file_url, stream=True)
+        logger.debug('Saving {0} to {1}'.format(
+            zip_file_basename, zip_file.name))
+        for chunk in r.iter_content(chunk_size=chunk_size):
+            zip_file.write(chunk)
+        logger.debug('Finished saving {0} to {1}'.format(
+            zip_file_basename, zip_file.name))
         zip_file.seek(0)
 
         logger.debug('Saving {0} to database'.format(zip_file_basename))
@@ -76,24 +70,17 @@ async def load_file_from_url(zip_file_url, engine, session):
         logger.debug('Completed {0}'.format(zip_file_basename))
 
 
-async def main(raw_args):
+def main(raw_args):
     args = parser.parse_args(raw_args)
 
     engine = create_engine(args.database_url)
 
-    conn = aiohttp.TCPConnector(limit=1)
-    async with aiohttp.ClientSession(connector=conn) as session:
-        file_urls = await get_file_urls(args.file_listing_url, session)
-        logger.info('Found {0} files to download'.format(len(file_urls)))
+    file_urls = get_file_urls(args.file_listing_url)
+    logger.info('Found {0} files to download'.format(len(file_urls)))
 
-        async def load_url(file_url):
-            await load_file_from_url(file_url, engine, session)
-
-        await asyncio.gather(*map(load_url, file_urls))
+    for file_url in file_urls:
+        load_file_from_url(file_url, engine)
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(sys.argv[1:]))
-    loop.run_until_complete(asyncio.sleep(1))
-    loop.close()
+    main(sys.argv[1:])
